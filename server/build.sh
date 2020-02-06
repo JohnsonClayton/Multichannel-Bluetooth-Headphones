6a7e1d558e610469596812702ff8a8a204460dd0
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-02-06 15:00:52 -0700

    make build.sh clean space

--- server/build.sh
+++ server/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Cleans the workspace
+rm -r voice/
+rm server.jar
+
+# Builds the jar file
 javac -d . VoiceServer.java Helper.java ClientHandler.java
 jar cvmf MANIFEST.MF server.jar voice/server/*
